0e26ee8cc6905c616ad60e09c123b7c22654621d
Felipe Kawassaki <felipe.kawassaki@olx.com>
2025-06-20 16:18:19 +0100

    fix npm test script

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ const createJestConfig = nextJest({ dir: "." });
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
   globalSetup: "<rootDir>/jest.setup.js",
+  testTimeout: 60000, // 60 seconds
 });
 
 module.exports = jestConfig;
