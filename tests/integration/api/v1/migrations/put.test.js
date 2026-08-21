08b10fc19dff91e68cf348b9752715ad2501e3f1
Felipe Kawassaki <felipe.kawassaki@olx.com>
2026-08-21 16:40:57 +0100

    fix: lint

--- tests/integration/api/v1/migrations/put.test.js
+++ tests/integration/api/v1/migrations/put.test.js
@@ -8,7 +8,9 @@ beforeAll(async () => {
 describe("PUT /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     test("Retrieving pending migrations", async () => {
-      const response = await fetch("http://localhost:3000/api/v1/migrations", { method: "PUT" });
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "PUT",
+      });
       expect(response.status).toBe(405);
 
       const responseBody = await response.json();
