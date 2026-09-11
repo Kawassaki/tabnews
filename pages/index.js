f6f978c07194fa735415273c72b8f8809a47e1e1
Felipe Kawassaki <felipe.kawassaki@olx.com>
2026-09-11 15:22:14 +0100

    feat: add default layout on home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import DefaultLayout from "../interface/DefaultLayout";
+
 function Home() {
-  return <h1>Lorayne, se você me ama, de uma risadinha! 🏀</h1>;
+  return (
+    <DefaultLayout
+      metadata={{
+        title: "Home",
+        description: "Welcome to Kawassaki",
+      }}
+    >
+      <h1>Lorayne, se você me ama, de uma risadinha! 🏀</h1>
+    </DefaultLayout>
+  );
 }
 
 export default Home;
